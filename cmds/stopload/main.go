add4f478189440e2575518e79e6eae12a1ccc50c
Brian Seitz <brnstz@gmail.com>
2015-07-10 16:26:11 -0400

    stopload pulls from web

--- cmds/stopload/main.go
+++ cmds/stopload/main.go
@@ -1,9 +1,10 @@
 package main
 
 import (
-	"flag"
+	"io/ioutil"
 	"log"
 	"os"
+	"os/exec"
 	"os/signal"
 	"path"
 	"strings"
@@ -15,33 +16,29 @@ import (
 	_ "github.com/lib/pq"
 )
 
-func doOne(dir string, db *sqlx.DB) {
-
-	stype := ""
-	if strings.HasSuffix(dir, "subway/") {
-		stype = "subway"
-	} else {
-		stype = "bus"
-	}
+func doOne(dir string, stype string, db *sqlx.DB) {
 
 	log.Println(dir, stype)
 	l := loader.NewLoader(dir)
 
-	for _, s := range l.ServiceRouteDays {
-		log.Println("Inserting service route day: ", s)
+	for i, s := range l.ServiceRouteDays {
 		_, err := db.Exec(`
 				INSERT INTO service_route_day
 				(route_id, service_id, day, start_date, end_date)
 				VALUES($1, $2, $3, $4, $5)
 			`, s.RouteId, s.ServiceId, s.Day, s.StartDate, s.EndDate,
 		)
-		if err != nil {
+		if err != nil && !strings.Contains(err.Error(), "violates unique constraint") {
 			log.Println("ERROR SERVICE ROUTE DAYS: ", err, s)
 		}
+
+		if i%100 == 0 {
+			log.Printf("loaded %v service route days", i)
+		}
+
 	}
 
-	for _, s := range l.Stops {
-		log.Println("Inserting stop: ", s)
+	for i, s := range l.Stops {
 		_, err := db.Exec(`
 				INSERT INTO stop
 				(stop_id, stop_name, direction_id, headsign, route_id,
@@ -52,31 +49,35 @@ func doOne(dir string, db *sqlx.DB) {
 			s.Lat, s.Lon, stype,
 		)
 
-		if err != nil {
+		if err != nil && !strings.Contains(err.Error(), "violates unique constraint") {
 			log.Println("ERROR STOPS: ", err, s)
+		}
+
+		if i%100 == 0 {
+			log.Printf("loaded %v stops", i)
 		}
 	}
 
-	for _, s := range l.ScheduledStopTimes {
-		log.Println("Inserting scheduled stop time: ", s)
+	for i, s := range l.ScheduledStopTimes {
 		_, err := db.Exec(`
 				INSERT INTO scheduled_stop_time
 				(route_id, stop_id, service_id, departure_sec)
 				VALUES($1, $2, $3, $4)
 			`, s.RouteId, s.StopId, s.ServiceId, s.DepartureSec,
 		)
-		if err != nil {
+		if err != nil && !strings.Contains(err.Error(), "violates unique constraint") {
 			log.Println("ERROR SCHEDULED STOP TIMES: ", err, s)
+		}
+
+		if i%100000 == 0 {
+			log.Printf("loaded %v stop times", i)
 		}
 	}
 }
 
 func main() {
+	log.SetFlags(log.Ltime | log.Lshortfile | log.Ldate)
 	db := common.DB
-	root := flag.String("dir", "", "directory with extracted mta data files")
-	if root == nil {
-		panic("must provide -dir")
-	}
 
 	// Dump stack trace on kill
 	c := make(chan os.Signal, 1)
@@ -88,20 +89,45 @@ func main() {
 		}
 	}()
 
-	for _, subdir := range []string{
-		"subway/",
-		"brooklyn/",
-		"manhattan/",
-		"queens/",
-		"staten_island/",
-		"bronx/",
+	for _, url := range []string{
+		"http://web.mta.info/developers/data/nyct/subway/google_transit.zip",
+		"http://web.mta.info/developers/data/nyct/bus/google_transit_bronx.zip",
+		"http://web.mta.info/developers/data/nyct/bus/google_transit_brooklyn.zip",
+		"http://web.mta.info/developers/data/nyct/bus/google_transit_manhattan.zip",
+		"http://web.mta.info/developers/data/nyct/bus/google_transit_queens.zip",
+		"http://web.mta.info/developers/data/nyct/bus/google_transit_staten_island.zip",
 	} {
-		dir := path.Join(root, subdir)
+		var stype string
+		if strings.Contains(url, "subway") {
+			stype = "subway"
+		} else {
+			stype = "bus"
+		}
 
-		t1 := time.Now()
-		doOne(dir, db)
-		t2 := time.Now()
-		log.Printf("took %v for %v\n", t2.Sub(t1), dir)
+		// FIXME: do this in go, need to make it integrated with loader
+		dir, err := ioutil.TempDir(os.Getenv("BUS_TMP_DIR"), "")
+		if err != nil {
+			panic(err)
+		}
+		cmd := exec.Command("/usr/bin/wget", url, "-O", path.Join(dir, "file.zip"))
+		err = cmd.Run()
+		if err != nil {
+			panic(err)
+		}
+
+		cmd = exec.Command("/usr/bin/unzip", path.Join(dir, "file.zip"), "-d", dir)
+		err = cmd.Run()
+		if err != nil {
+			panic(err)
+		}
+
+		func() {
+			defer os.RemoveAll(dir)
+			t1 := time.Now()
+			doOne(dir, stype, db)
+			t2 := time.Now()
+			log.Printf("took %v for %v\n", t2.Sub(t1), dir)
+		}()
 	}
 
 	log.Println("finished all boroughs")
