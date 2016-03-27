78f993218a66edcc676494d607b07abbd3bfea91
Brian Seitz <brnstz@gmail.com>
2016-03-26 23:10:30 -0400

    use port

--- automation/ci.sh
+++ automation/ci.sh
@@ -6,4 +6,5 @@ psql -U $BUS_DB_USER < ../schema/schema.sql
 go install github.com/brnstz/bus/cmds/busapi
 $GOPATH/bin/busapi &
 sleep 2
-curl -i "http://$BUS_API_ADDR/api/v1/stops?lat=40.729183&lon=-73.95154&&miles=0.5&filter=subway"
+port=`echo $BUS_API_ADDR | cut -f2 -d:`
+curl -i "http://localhost:$port/api/v1/stops?lat=40.729183&lon=-73.95154&&miles=0.5&filter=subway"
