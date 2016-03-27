71c6f5ae04cfd31b130140eff0426c54c8adad4a
Brian Seitz <brnstz@gmail.com>
2016-03-26 23:03:58 -0400

    use correct var

--- automation/ci.sh
+++ automation/ci.sh
@@ -6,4 +6,4 @@ psql -U $BUS_DB_USER < ../schema/schema.sql
 go install github.com/brnstz/bus/cmds/busapi
 $GOPATH/bin/busapi &
 sleep 2
-curl -i "http://localhost:$BUS_API_PORT/api/v1/stops?lat=40.729183&lon=-73.95154&&miles=0.5&filter=subway"
+curl -i "http://$BUS_API_ADDR/api/v1/stops?lat=40.729183&lon=-73.95154&&miles=0.5&filter=subway"
