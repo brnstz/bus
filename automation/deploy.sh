963bf878a1a7cdbee8e42ff44583a51c6d2da956
Brian Seitz <brnstz@gmail.com>
2016-03-28 00:04:24 -0400

    startup fixes

--- automation/deploy.sh
+++ automation/deploy.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 export ANSIBLE_HOST_KEY_CHECKING=False
+export VAULT_PASSWORD_FILE="~/.pwd"
 
 if [ $# -lt 2 ]; then
     echo "Usage: $0 inventory playbook-1.yml [playbook-2.yml ... ]"
-    echo "Example: $0 inventory_vagrant api.yml"
+    echo "Example: $0 inventory_vagrant db.yml api.yml"
     exit 1
 fi
 
-ansible-playbook -v -i $1 ${@:2}
+cd `dirname $0` && 
+ansible-playbook -v --vault-password-file=$VAULT_PASSWORD_FILE -e "inventory=$1" -i $1 ${@:2}
