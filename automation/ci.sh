27b7175b7b096116460d7cac2c2d632f751904dc
Brian Seitz <brnstz@gmail.com>
2016-04-02 15:17:44 -0400

    check for api response in ci

--- automation/ci.sh
+++ automation/ci.sh
@@ -1,13 +1,26 @@
 #!/bin/bash
 
-cd `dirname $0` || exit 1
+cleanup() {
+    killall -9 busapi
+}
 
-psql -U $BUS_DB_USER -d $BUS_DB_NAME < ../schema/schema.sql || exit 1
-go install github.com/brnstz/bus/cmds/busapi || exit 1
-go install github.com/brnstz/bus/cmds/busloader || exit 1
+error() {
+    cleanup
+    exit 1
+}
 
-$GOPATH/bin/busloader || exit 1
+cd `dirname $0` || error
 
-#port=`echo $BUS_API_ADDR | cut -f2 -d:`
+psql -U $BUS_DB_USER -d $BUS_DB_NAME < ../schema/schema.sql || error
+go install github.com/brnstz/bus/cmds/busapi || error
+go install github.com/brnstz/bus/cmds/busloader || error
 
-#curl -i "http://localhost:$port/api/v1/stops?lat=40.729183&lon=-73.95154&&miles=0.5&filter=subway"
+$GOPATH/bin/busloader || error
+$GOPATH/bin/busapi & 
+
+sleep 2
+
+port=`echo $BUS_API_ADDR | cut -f2 -d:`
+curl -i "http://localhost:$port/api/v1/stops?lat=40.729183&lon=-73.95154&&miles=0.5&filter=subway" | grep 'Greenpoint Av' || exit 1
+
+cleanup
