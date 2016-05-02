a2f572e498f80c2160ecb6d75da6004311201fcf
Brian Seitz <brnstz@gmail.com>
2016-05-02 12:49:09 -0400

    adding leaflet map etc.

--- web/js/bus.js
+++ web/js/bus.js
@@ -1,24 +1,60 @@
 var bus = new Bus();
 
 function Bus() {
+    // lat, lon is the center of our request. We send this to the Bus API
+    // and also use it to draw the map. We can get this value from the
+    // HTML5 location API.
     this.lat = 0;
     this.lon = 0;
+
+    // miles and filter are options sent to the Bus API
     this.miles = 0.5;
     this.filter = '';
+
+    // tileURL is passed to Leaflet JS for drawing the map
+    this.tileURL = 'http://otile1.mqcdn.com/tiles/1.0.0/map/{z}/{x}/{y}.png';
+
+    // tileOptions is passed to Leatlef JS for drawing the map
+    this.tileOptions = {
+        MaxZoom: 20
+    };
+
+    // zoom is the zoom value when drawing the Leaflet map
+    this.zoom = 15;
+
+    // map is our Leaflet JS map object
+    this.map = null;
 }
+
+// init is run when the page initially loads
+Bus.prototype.init = function() {
+    this.refresh();
+};
 
 // refresh requests the location from the browser, sets our lat / lon and
 // gets new trips from the API 
 Bus.prototype.refresh = function() {
     var self = this;
     if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(function(position) {
-            self.lat = position.coords.latitude;
-            self.lon = position.coords.longitude;
-            self.getTrips();
+        navigator.geolocation.getCurrentPosition(function(p) {
+            self.updatePosition(p)
         });
     }
 };
+
+
+// updatePosition takes an HTML5 geolocation position response and 
+// updates our map and trip info
+Bus.prototype.updatePosition = function(position) {
+    this.lat = position.coords.latitude;
+    this.lon = position.coords.longitude;
+
+    this.map = L.map('map').setView([this.lat, this.lon], this.zoom);
+    L.tileLayer(this.tileURL, this.tileOptions).addTo(this.map);
+
+    this.getTrips();
+};
+
 
 // appendCell creates a td cell with the value and appends it to row
 Bus.prototype.appendCell = function(row, value) {
@@ -42,24 +78,35 @@ Bus.prototype.appendCellColor = function(row, value, fgcolor, bgcolor) {
     row.appendChild(cell);
 };
 
+// hourFormat takes a time object and returns the format we want to display on
+// screen
+Bus.prototype.timeFormat = function(time) {
+    var t = new Date(time);
+
+    // Get minutes as a 00 padded value
+    var minutes = ("00" + t.getMinutes()).slice(-2);
+
+    // Get a temporary value for hours, format it below
+    var hours = t.getHours();
+
+    // Convert to US time
+    if (hours > 12) {
+        hours -= 12;
+    }
+    if (hours == 0) {
+        hours = 12;
+    }
+
+    return hours + ":" + minutes;
+}
+
 // appendTime adds a cell to this row with the current time values
-Bus.prototype.appendTime = function(row, times) {
+Bus.prototype.appendTime = function(row, departures) {
     var mytext = "";
 
-    if (times != null) {
-        for (var i = 0; i < times.length; i++) {
-            var mytime = new Date(times[i].time);
-            var h = mytime.getHours();
-            var ampm = "am";
-            if (h >= 12) {
-                h = h - 12;
-                ampm = "pm";
-            } else if (h == 0) {
-                h = 12;
-            }
-            var hour = ("00" + h).slice(-2);
-            var minute = ("00" + mytime.getMinutes()).slice(-2);
-            mytext = mytext + " " + hour + ":" + minute + ampm;
+    if (departures != null) {
+        for (var i = 0; i < departures.length; i++) {
+            mytext += " " + this.timeFormat(departures[i].time);
         }
     }
 
