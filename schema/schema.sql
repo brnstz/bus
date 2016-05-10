e2e17d139e1fbea34458aa8373d49779ceb671d9
Brian Seitz <brnstz@gmail.com>
2016-05-10 06:28:15 -0400

    adding agency, trip shape to schema

--- schema/schema.sql
+++ schema/schema.sql
@@ -7,21 +7,26 @@ CREATE EXTENSION IF NOT EXISTS earthdistance;
 -- route contains selected fields from 
 -- https://developers.google.com/transit/gtfs/reference#routestxt
 CREATE TABLE route (
+    agency_id           TEXT NOT NULL,
+
     -- route_id is the unique id of this route
-    route_id            TEXT NOT NULL PRIMARY KEY,
+    route_id            TEXT NOT NULL,
 
     -- route_type is an id code for whether it's a subway, bus, etc. 
     -- (e.g., subway=1, bus=3, see link above for full doc)
     route_type          INT NOT NULL,
 
     -- route_color and route_text_color are the hex values for the background
-    -- and foreground color of the route, respectively (e.g., #000000)
+    -- and foreground color of the route, respectively (e.g., 000000)
     route_color         TEXT NOT NULL,
     route_text_color    TEXT NOT NULL
+
+    UNIQUE(agency_id, route_id)
 );
 
 CREATE TABLE stop (
     -- the unique id and name of the stop
+    agency_id TEXT NOT NULL,
     stop_id   TEXT NOT NULL,
     stop_name TEXT NOT NULL,
 
@@ -37,37 +42,54 @@ CREATE TABLE stop (
     -- lat and lon converted into an earthdistance type
     location    EARTH NOT NULL,
 
-    UNIQUE(route_id, stop_id)
+    UNIQUE(agency_id, route_id, stop_id)
 );
 CREATE INDEX idx_location_stop ON stop USING gist(location);
 
 CREATE TYPE day_type AS ENUM ('monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday');
 
 CREATE TABLE service_route_day (
+    agency_id   TEXT NOT NULL,
     route_id    TEXT NOT NULL,
     service_id  TEXT NOT NULL,
     day         DAY_TYPE NOT NULL,
     start_date  DATE NOT NULL,
     end_date    DATE NOT NULL,
 
-    UNIQUE(route_id, service_id, day, start_date, end_date)
+    UNIQUE(agency_id, route_id, service_id, day, start_date, end_date)
 );
 
+CREATE TABLE trip (
+    trip_id     TEXT NOT NULL PRIMARY KEY,
+    service_id  TEXT NOT NULL,
+    shape_id    TEXT NOT NULL
+)
+
+CREATE TABLE shape (
+    shape_id TEXT NOT NULL,
+    location EARTH NOT NULL,
+    seq      INT NOT NULL,
+
+    UNIQUE(shape_id, seq)
+)
+
 CREATE TABLE service_route_exception (
+    agency_id       TEXT NOT NULL,
     route_id        TEXT NOT NULL,
     service_id      TEXT NOT NULL,
     exception_date  DATE NOT NULL,
 
-    UNIQUE(route_id, service_id, exception_date)
+    UNIQUE(agency_id, route_id, service_id, exception_date)
 );
 
 CREATE TABLE scheduled_stop_time (
+    agency_id      TEXT NOT NULL,
     route_id       TEXT NOT NULL,
     stop_id        TEXT NOT NULL,
     service_id     TEXT NOT NULL,
     departure_sec  INT  NOT NULL,
     
-    UNIQUE(route_id, stop_id, service_id, departure_sec)
+    UNIQUE(agency_id, route_id, stop_id, service_id, departure_sec)
 );
 -- CREATE INDEX idx_sst ON scheduled_stop_time (route_id, stop_id, service_id, departure_sec);
 -- select route_id, service_id, max(start_day) from service_route_day where route_id = 'G' and day = 'monday' group by route_id, service_id;
