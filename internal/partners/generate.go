044374a9f13945e217442d63590039b725012b3a
Brian Seitz <brnstz@gmail.com>
2016-07-07 13:13:41 -0400

    add comment to proto buff generate command

--- internal/partners/generate.go
+++ internal/partners/generate.go
@@ -1,4 +1,7 @@
 package partners
 
+// This will regenerate the *.pb.go files in transit_realtime/ and nyct_subway/
+// (But those files are also stored in git)
+
 //go:generate protoc --proto_path=./ --proto_path=../../../../../ --go_out=./ nyct_subway/nyct-subway.proto
 //go:generate protoc --go_out=./ transit_realtime/gtfs-realtime.proto
