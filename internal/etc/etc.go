753894476b2b4d14aa7b83741ea56a7420f6b5e9
Brian Seitz <brnstz@gmail.com>
2016-07-13 18:01:35 -0400

    more refactoring

--- internal/etc/etc.go
+++ internal/etc/etc.go
@@ -140,3 +140,15 @@ func MustDB() *sqlx.DB {
 
 	return db
 }
+
+// BaseTime takes a time and returns the same time with the hour, minute, second
+// and nanosecond values set to zero, so that it represents the start
+// of the day
+func BaseTime(t time.Time) time.Time {
+	t = t.Add(-time.Hour * time.Duration(t.Hour()))
+	t = t.Add(-time.Minute * time.Duration(t.Minute()))
+	t = t.Add(-time.Second * time.Duration(t.Second()))
+	t = t.Add(-time.Nanosecond * time.Duration(t.Nanosecond()))
+
+	return t
+}
