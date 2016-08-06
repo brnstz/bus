bf2f6a279053ab64465586787ac0a188a96d088d
Brian Seitz <brnstz@gmail.com>
2016-08-05 20:25:14 -0400

    adding migration for fake shape table

---
+++ migrations/V15__fake_shape.sql
@@ -0,0 +1,10 @@
+CREATE TABLE fake_shape (
+    agency_id       TEXT NOT NULL,
+    route_id        TEXT NOT NULL,
+    direction_id    INT NOT NULL,
+    headsign        TEXT NOT NULL,
+    location        GEOMETRY NOT NULL,
+    seq             INT NOT NULL,
+
+    UNIQUE(agency_id, route_id, direction_id, headsign)
+);
