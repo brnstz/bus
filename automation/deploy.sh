6a134ea2fa9cce26477e4b9835f706942260cc17
Brian Seitz <brnstz@gmail.com>
2016-08-25 14:10:14 -0400

    adding deployer build

--- automation/deploy.sh
+++ automation/deploy.sh
@@ -10,4 +10,4 @@ if [ $# -lt 2 ]; then
 fi
 
 cd `dirname $0` && 
-ansible-playbook -v --vault-password-file=$VAULT_PASSWORD_FILE --ssh-extra-args="-o ControlMaster=no -o ControlPath=none -o ControlPersist=no" -e "inventory=$1" -i $1 ${@:2}
+ansible-playbook --vault-password-file=$VAULT_PASSWORD_FILE -e "inventory=$1" -i $1 ${@:2}
